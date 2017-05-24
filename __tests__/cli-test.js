eca6aa8b3e660d526660fb90c28a6d2bb3f58f26
Rodolphe Stoclin <srodolphe@gmail.com>
2017-05-24 11:09:37 +0200

    Move cli test file and fix spy error

--- __tests__/cli-test.js
+++ __tests__/cli-test.js
@@ -7,17 +7,14 @@ var cli = require('../lib/cli');
 var utils = require('../lib/utils');
 
 describe('cli', function() {
-  beforeEach(function() {
-    this.spy = jest.spyOn(nodeMinify, 'minify');
-  });
-
   test('should minify to have been called', function() {
+    const spy = jest.spyOn(nodeMinify, 'minify');
     cli({
       compressor: 'gcc',
       input: 'examples/public/js/sample.js',
       output: 'examples/public/js-dist/babili-es6.js'
     });
-    expect(this.spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
   /*test('should minify to have been called with all compressors', function() {
     cli({
